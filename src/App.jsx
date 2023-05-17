43d74d9f3bedacdc0c2103a3a3ce6b87da32f27b
Danilo Gomes <danilogomes@fieg.com.br>
2023-05-17 10:05:00 -0300

    Add formulario de comentarios e ajustes no codigo.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import styles from './App.module.css'
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { Post } from './components/Post';
+import { Comment } from './components/Comment';
 
 function App() {
   return (
