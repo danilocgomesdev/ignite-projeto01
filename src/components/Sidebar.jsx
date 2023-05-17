908c467081d866136e7870414e8460de2bed524d
Danilo Gomes <danilogomes@fieg.com.br>
2023-05-17 09:21:22 -0300

    inserindo o componente avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -8,10 +10,7 @@ return (
             className={styles.cover}
             src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" />
         <div className={styles.profile}>
-        <img 
-        className={styles.avatar}
-        src="https://github.com/danilocgomesdev.png" 
-        />
+        <Avatar src="https://github.com/danilocgomesdev.png" />
         <strong>Danilo Gomes</strong>
         <span>Web Developer</span>
         </div>
